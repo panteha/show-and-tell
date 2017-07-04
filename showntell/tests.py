847ffdc38b06e102ff7ba15e162d36a703cd5d67
Lubos Michalic <lnmichalic@gmail.com>
2017-07-04 23:51:48 +0100

    Add basic view to satisfy unit and feature tests

--- showntell/tests.py
+++ showntell/tests.py
@@ -1,6 +1,21 @@
 from django.test import TestCase
+from django.core.urlresolvers import resolve
+from django.test import TestCase
+from django.http import HttpRequest
 
+from showntell.views import home_page
 # Create your tests here.
 class SmokeTest(TestCase):
     def test_unit_tests_work(self):
         self.assertEquals(1,1)
+class HomePageTest(TestCase):
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEquals(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>ShowNTell</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
