979c8924f43ee29b6100cc33a0f266ddca17ee21
Panteha <panteha.ahmadi@gmail.com>
2017-07-04 23:06:26 +0100

    Added Root to ObjectDetector

--- object_detect/__init__.py
+++ object_detect/__init__.py
@@ -13,21 +13,22 @@ from PIL import Image
 
 # This is needed to display the images.
 # %matplotlib inline
-sys.path.append('/Users/Pani/Documents/MakersAcademy/show-and-tell/object_detect/models')
+ROOT = '/Users/Pani/Documents/MakersAcademy/show-and-tell/object_detect/'
+sys.path.append(ROOT + '/models')
 
 from object_detection.utils import label_map_util
 
 from object_detection.utils import visualization_utils as vis_util
 
 MODEL_NAME = 'ssd_mobilenet_v1_coco_11_06_2017'
-MODEL_FILE = MODEL_NAME + '.tar.gz'
+MODEL_FILE = ROOT + MODEL_NAME + '.tar.gz'
 DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
 
 # Path to frozen detection graph. This is the actual model that is used for the object detection.
-PATH_TO_CKPT = MODEL_NAME + '/frozen_inference_graph.pb'
+PATH_TO_CKPT = ROOT + MODEL_NAME + '/frozen_inference_graph.pb'
 
 # List of the strings that is used to add correct label for each box.
-PATH_TO_LABELS = os.path.join('models/object_detection/data', 'mscoco_label_map.pbtxt')
+PATH_TO_LABELS = os.path.join(ROOT + 'models/object_detection/data', 'mscoco_label_map.pbtxt')
 
 NUM_CLASSES = 90
 
