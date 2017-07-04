133d4933aa09cc57c0b77f99fcc08d75a15b7e88
Alex Satur <alex.satur89@gmail.com>
2017-07-04 15:25:52 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,7 +3,7 @@ from flask import Flask, request, redirect, url_for, render_template
 from werkzeug.utils import secure_filename
 from flask import send_from_directory
 
-UPLOAD_FOLDER = os.getcwd() + '/showNtell/images/uploads'
+UPLOAD_FOLDER = os.getcwd() + '/images/uploads'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
