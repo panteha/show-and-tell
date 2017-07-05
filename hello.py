9034502f5a7d5267c22af1ad2befb82190a22fa2
Panteha <panteha.ahmadi@gmail.com>
2017-07-05 16:48:41 +0100

    Defined the path for the uploaded photo inside uploaded_file method

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask, request, redirect, url_for, render_template
 from werkzeug.utils import secure_filename
 from flask import send_from_directory
-from object_detect import FakeObjectDetector
+from object_detect import ObjectDetector
 from PIL import Image
 
 UPLOAD_FOLDER = os.getcwd() + '/images/uploads'
@@ -27,16 +27,18 @@ def upload_file():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            global image_location
+            image_location = UPLOAD_FOLDER + '/' + file.filename
             return redirect(url_for('uploaded_file', filename=filename))
     return render_template('upload.html')
 
 @app.route('/images/uploads/<filename>')
 def uploaded_file(filename):
     # return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
-    fod = FakeObjectDetector()
-    image = Image.open('/Users/Pani/Documents/MakersAcademy/show-and-tell/images/uploads/airplane.jpg')
-    image_np = fod.load_image_into_numpy_array(image)
-    return ','.join(fod.detect(image_np))
+    objectdetect = ObjectDetector()
+    image = Image.open(image_location)
+    image_np = objectdetect.load_image_into_numpy_array(image)
+    return ','.join(objectdetect.detect(image_np))
 
 @app.route('/hello')
 def hello(name=None):
